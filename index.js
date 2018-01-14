1b12cc216e664446ab4c927a6a7629f349b4240a
Greg Hesp <greg.hesp@gmail.com>
2018-01-14 08:38:01 +0000

    Testing multiuser

--- index.js
+++ index.js
@@ -13,117 +13,142 @@ const ssdpServer = new SSDP({
   ssdpTtl: 3,
 });
 
-//Define Google Assistant Configuration
+const authKeys = GRConfig.users;
+
 const config = {
-  auth: {
-    keyFilePath: path.resolve(__dirname, `${GRConfig.keyFile}`),
-    savedTokensPath: path.resolve(__dirname, 'tokens.json'),
-  },
   conversation: {
-    lang: 'en-US', // defaults to en-US, but try other ones, it's fun!
+    lang: GRConfig.language,
   },
-};
-
-//Define SSDP USN type
-ssdpServer.addUSN('urn:greghesp-com:device:GAssist:1');
-
-const app = express()
-app.use(express.static(path.join(__dirname, 'xml')));
-
-//Start SSDP Server
-ssdpServer.start(function(){
-  console.log('Fired up the SSDP Server for network discovery...')
-});
-
-// Endpoint API
-app.post('/custom', function (req, res) {
-  sendTextInput(req.query.command)
-})
-
-app.post('/customBroadcast', function (req, res) {
-  sendTextInput(`broadcast ${req.query.text}`)
-})
-
-app.post('/nestStream', function (req, res) {
-  if(req.query.stop) return sendTextInput(`Stop ${chromecast}`)
-  sendTextInput(`Show ${req.query.camera} on ${chromecast}`)
-})
-
-app.post('/broadcast', function (req, res) {
-  const preset = req.query.preset;
-
-  switch(preset) {
-    case 'wakeup':
-        sendTextInput(`broadcast wake up everyone`);
-        break;
-    case 'breakfast':
-        sendTextInput(`broadcast breakfast is ready`);
-        break;
-    case 'lunch':
-        sendTextInput(`broadcast it's lunch time`);
-        break;
-    case 'dinner':
-        sendTextInput('broadcast dinner is served');
-        break;
-    case 'timetoleave':
-        sendTextInput(`broadcast its time to leave`);
-        break;
-    case 'arrivedhome':
-        sendTextInput(`broadcast i'm home`);
-        break;
-    case 'ontheway':
-        sendTextInput(`broadcast i'm on the way`);
-        break;
-    case 'movietime':
-        sendTextInput(`broadcast the movie is about to start`);
-        break;
-    case 'tvtime':
-        sendTextInput(`broadcast the show is about to start`);
-        break;
-    case 'bedtime':
-        sendTextInput(`broadcast we should go to bed`);
-        break;
-    default:
-        sendTextInput(`broadcast you selected a broadcast in command but didn't specify one`);
-      }
-})
-
-//Start Express Web Server
-app.listen(3000, () => console.log('Firing up the Web Server for communication on port 3000'))
-
-//Assistant Integration
-const startConversation = (conversation) => {
-  conversation
-    .on('response', text => console.log('Assistant is responding', text))
-    //.on('volume-percent', percent => console.log('New Volume Percent:', percent))
-    //.on('device-action', action => console.log('Device Action:', action))
-    .on('ended', (error, continueConversation) => {
-      if (error) {
-        console.log('Conversation Ended Error:', error);
-      } else if (continueConversation) {
-        promptForInput();
-      } else {
-        console.log('Conversation Complete');
-        conversation.end();
-      }
-    })
-    .on('error', (error) => {
-      console.log('Conversation Error:', error);
-    });
-};
-
-const sendTextInput = (text) => {
-  if(!text) {
-    config.conversation.textQuery = 'broadcast Assistant Relay is now running. Enjoy!'
-  } else {
-    config.conversation.textQuery = text;
-  }
-  assistant.start(config.conversation, startConversation);
+  users:{}
 }
 
-const assistant = new GoogleAssistant(config.auth);
-  assistant
-    .on('ready', ()=> sendTextInput())
-    .on('error', (error) => {
-      console.log('Assistant Error:', error);
-    });
+for(var user in authKeys) {
+  if (authKeys.hasOwnProperty(user)) {
+    config.users[`${user}`] = {}
+
+    config.users[`${user}`].keyFilePath = path.resolve(__dirname, authKeys[user]);
+    config.users[`${user}`].savedTokensPath = path.resolve(__dirname,`${user}-tokens.json`);
+  }
+}
+
+var usersX = config.users;
+
+for(var assistant in usersX) {
+  const `${usersX}` = new GoogleAssistant(config.users[`${usersX}`]);
+}
+
+
+// const assistant = new GoogleAssistant(config.auth);
+//   assistant
+//     .on('ready', ()=> sendTextInput())
+//     .on('error', (error) => {
+//       console.log('Assistant Error:', error);
+//     });
+
+
+
+
+//
+// //Define SSDP USN type
+// ssdpServer.addUSN('urn:greghesp-com:device:GAssist:1');
+//
+// const app = express()
+// app.use(express.static(path.join(__dirname, 'xml')));
+//
+// //Start SSDP Server
+// ssdpServer.start(function(){
+//   console.log('Fired up the SSDP Server for network discovery...')
+// });
+//
+// // Endpoint API
+// app.post('/custom', function (req, res) {
+//   sendTextInput(req.query.command)
+// })
+//
+// app.post('/customBroadcast', function (req, res) {
+//   sendTextInput(`broadcast ${req.query.text}`)
+// })
+//
+// app.post('/nestStream', function (req, res) {
+//   if(req.query.stop) return sendTextInput(`Stop ${chromecast}`)
+//   sendTextInput(`Show ${req.query.camera} on ${chromecast}`)
+// })
+//
+// app.post('/broadcast', function (req, res) {
+//   const preset = req.query.preset;
+//
+//   switch(preset) {
+//     case 'wakeup':
+//         sendTextInput(`broadcast wake up everyone`);
+//         break;
+//     case 'breakfast':
+//         sendTextInput(`broadcast breakfast is ready`);
+//         break;
+//     case 'lunch':
+//         sendTextInput(`broadcast it's lunch time`);
+//         break;
+//     case 'dinner':
+//         sendTextInput('broadcast dinner is served');
+//         break;
+//     case 'timetoleave':
+//         sendTextInput(`broadcast its time to leave`);
+//         break;
+//     case 'arrivedhome':
+//         sendTextInput(`broadcast i'm home`);
+//         break;
+//     case 'ontheway':
+//         sendTextInput(`broadcast i'm on the way`);
+//         break;
+//     case 'movietime':
+//         sendTextInput(`broadcast the movie is about to start`);
+//         break;
+//     case 'tvtime':
+//         sendTextInput(`broadcast the show is about to start`);
+//         break;
+//     case 'bedtime':
+//         sendTextInput(`broadcast we should go to bed`);
+//         break;
+//     default:
+//         sendTextInput(`broadcast you selected a broadcast in command but didn't specify one`);
+//       }
+// })
+//
+// //Start Express Web Server
+// app.listen(3000, () => console.log('Firing up the Web Server for communication on port 3000'))
+//
+// //Assistant Integration
+// const startConversation = (conversation) => {
+//   conversation
+//     .on('response', text => console.log('Assistant is responding', text))
+//     //.on('volume-percent', percent => console.log('New Volume Percent:', percent))
+//     //.on('device-action', action => console.log('Device Action:', action))
+//     .on('ended', (error, continueConversation) => {
+//       if (error) {
+//         console.log('Conversation Ended Error:', error);
+//       } else if (continueConversation) {
+//         promptForInput();
+//       } else {
+//         console.log('Conversation Complete');
+//         conversation.end();
+//       }
+//     })
+//     .on('error', (error) => {
+//       console.log('Conversation Error:', error);
+//     });
+// };
+//
+// const sendTextInput = (text) => {
+//   if(!text) {
+//     config.conversation.textQuery = 'broadcast Assistant Relay is now running. Enjoy!'
+//   } else {
+//     config.conversation.textQuery = text;
+//   }
+//   assistant.start(config.conversation, startConversation);
+// }
+//
+// const assistant = new GoogleAssistant(config.auth);
+//   assistant
+//     .on('ready', ()=> sendTextInput())
+//     .on('error', (error) => {
+//       console.log('Assistant Error:', error);
+//     });
