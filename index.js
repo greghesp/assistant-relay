966cb5c5a983f76c4ce6c0a39d750941af81df7d
Greg Hesp <greg.hesp@gmail.com>
2018-01-20 19:54:54 +0000

    test

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const GRConfig = require('./config.json');
 const readline = require('readline');
 const async = require('async');
 const loader = require('audio-loader');
-//const updater = require('./updateChecker.js');
+const updater = require('./updateChecker.js');
 
+//test comment
 
 //Define SSDP Server Configuration
 const ssdpServer = new SSDP({
