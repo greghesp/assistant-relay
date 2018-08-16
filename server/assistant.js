64275687566ffa019ed6aebc0d6c1b41aef89650
Greg Hesp <greg.hesp@gmail.com>
2018-08-16 14:32:26 +0100

    Added http responses

--- server/assistant.js
+++ server/assistant.js
@@ -1,5 +1,6 @@
 const async = require('async');
 const GoogleAssistant = require('google-assistant');
+const FileWriter = require('wav').FileWriter;
 
 const defaultAudio = false;
 let returnAudio;
@@ -22,21 +23,98 @@ var self = module.exports = {
       }, function(err){
         if(err) return reject(err.message);
         console.log(`Assistant Relay is now setup and running for${users.map(u => ` ${u}`)}`)
-        resolve('broadcast Assistant Relay is now setup and running')
+        resolve()
+        //resolve('broadcast Assistant Relay is now setup and running')
       })
     })
   },
 
   sendTextInput: function(text, n, config, converse) {
-    console.log(`Received command ${text}`);
-    if(converse) returnAudio = true;
-    gConfig = config;
+    return new Promise((resolve, reject) => {
+      gConfig = config
+      console.log(`Received command ${text}`);
+      if(converse) returnAudio = true;
+      // set the conversation query to text
+      gConfig.conversation.textQuery = text;
+      const assistant = self.setUser(gConfig, n)
+
+       assistant.start(config.conversation, (conversation) => {
+          return self.startConversation(conversation)
+          .then((data) => {
+            resolve(data)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+        });
+    })
+  },
+
+  // audioTest: function(text, n, config, audio) {
+  //   console.log(`Received command ${text}`);
+  //   return new Promise(resolve => {
+  //     config.conversation.textQuery = text;
+  //     const assistant = self.setUser(config, n)
+  //
+  //      assistant.start(config.conversation, (conversation) => {
+  //         return self.startConversation(conversation, audio)
+  //       });
+  //   })
+  // },
+
+  startConversation: function(conversation, outputFileStream) {
+    let response = {};
+    return new Promise((resolve, reject) => {
+      conversation
+        // .on('audio-data', data => {
+        //   if(outputFileStream) outputFileStream.write(data)
+        // })
+        .on('response', (text) => {
+          if (text) {
+            console.log('Google Assistant:', text)
+            response.response = text;
+            if(returnAudio) {
+              self.sendTextInput(`broadcast ${text}`, null, gConfig);
+              returnAudio = false;
+            }
+          }
+        })
+        .on('volume-percent', percent => {
+          console.log('New Volume Percent:', percent)
+          response.response = `New Volume Percent is ${percent}`;
+        })
+        .on('device-action', action => {
+          console.log('Device Action:', action)
+          response.response = `Device Action is ${action}`;
+        })
+        .on('ended', (error, continueConversation) => {
+          if (error) {
+            console.log('Conversation Ended Error:', error);
+            response.false = true;
+            reject(response)
+          } else if (continueConversation) {
+            response.success = true;
+            console.log('Continue the conversation... somehow');
+            conversation.end();
+            resolve();
+          } else {
+            response.success = true;
+            console.log('Conversation Complete');
+            // if(outputFileStream) outputFileStream.end()
+            conversation.end();
+            resolve(response);
+          }
+        })
+        .on('error', (error) => {
+          reject(error)
+        });
+    })
+  },
+
+  setUser: function(config, n) {
     // set default assistant to first user
     assistant = Object.keys(config.assistants)[0];
     assistant = config.assistants[`${assistant}`];
-
-    // set the conversation query to text
-    config.conversation.textQuery = text;
 
     // check to see if user passed exists
     if(n) {
@@ -51,44 +129,7 @@ var self = module.exports = {
     } else {
       console.log(`No user specified, using ${Object.keys(config.assistants)[0]}`)
     }
-     assistant.start(config.conversation, self.startConversation);
-  },
 
-  startConversation: function(conversation) {
-    let response;
-    return new Promise((resolve, reject) => {
-      conversation
-        //.on('audio-data', data => console.log('Got some audio data'))
-        .on('response', (text) => {
-          if (text) {
-            console.log('Google Assistant:', text)
-            response = text;
-            if(returnAudio) {
-              self.sendTextInput(`broadcast ${text}`, null, gConfig);
-              returnAudio = false;
-            }
-          }
-        })
-        .on('volume-percent', percent => console.log('New Volume Percent:', percent))
-        .on('device-action', action => console.log('Device Action:', action))
-        .on('ended', (error, continueConversation) => {
-          if (error) {
-            console.log('Conversation Ended Error:', error);
-            reject(error)
-          } else if (continueConversation) {
-            console.log('Continue the conversation... somehow');
-            conversation.end();
-            resolve();
-          } else {
-            console.log('Conversation Complete');
-            conversation.end();
-            resolve(response);
-          }
-        })
-        .on('error', (error) => {
-          console.log('Conversation Error:', error);
-          reject(error)
-        });
-    })
+    return assistant;
   }
 }
