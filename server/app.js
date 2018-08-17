fccdcf7d3285dd6dc5bfd4d6ce11b72e39bc7341
Greg Hesp <greg.hesp@gmail.com>
2018-08-17 21:18:39 +0100

    Added responses and audio recording

--- server/app.js
+++ server/app.js
@@ -1,102 +1,31 @@
 const GoogleAssistant = require('google-assistant');
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const FileWriter = require('wav').FileWriter;
 
-const ip = require('ip');
-
 const startConversation = require('./assistant.js')
-const configFile = require('./configurations/config.json');
+const routes = require('./routes.js');
+const dashboard = require('./dashboard.js');
+const audio = require('./audio.js');
+
 const configureUsers = require('./configuration').configureUsers;
 const setupConfigVar = require('./configuration').setupConfigVar;
 const setupAssistant = require('./assistant').setupAssistant;
 const sendTextInput = require('./assistant').sendTextInput;
-const audioTest = require('./assistant').audioTest;
-
-const directory = path.resolve()
 
 const app = express();
 //app.use(bodyParser.json());
-
-let config;
-const baseConfig = {
-  conversation: {
-    lang: configFile.language,
-    audio: {
-      sampleRateOut: 24000, // defaults to 24000
-    }
-  },
-  users: {},
-  assistants: {},
-  port: configFile.port
-}
+app.use('/', routes);
+app.use('/dashboard', dashboard)
+app.use('/audio', audio)
 
 // Configure users on first run
 configureUsers()
-.then(() => {
-  config = setupConfigVar(baseConfig, configFile.users);
-  return setupAssistant(config)
-})
-.then((string) => {
-  sendTextInput(string, null, config)
+.then((config) => {
+  global.config = config;
+  app.listen(global.config.port);
+  return setupAssistant()
 })
 .catch((e) => {
   console.log(e)
 })
-
-
-app.post('/customBroadcast', function (req, res) {
-  const command = req.body.text;
-  const user = req.body.user;
-
-  sendTextInput(`broadcast ${command}`, user, config);
-
-  res.status(200).json({
-      message: `Custom broadcast command executed`,
-      command: `broadcast ${command}`
-  });
-})
-
-app.post('/custom', function (req, res) {
-  const converse = req.query.converse;
-  const command = req.query.command;
-  const user = req.query.user;
-
-  //Check the converse parameter
-  sendTextInput(command, user, config, converse)
-  .then((response) => {
-    res.status(200).json({
-        message: `Custom command executed`,
-        command: `${command}`,
-        data: response
-    });
-  })
-})
-
-app.post('/audio', function (req, res) {
-  const command = req.query.command;
-  const user = req.query.user;
-  var outputFileStream = new FileWriter(path.resolve(__dirname, 'test.wav'), {
-    sampleRate: config.conversation.audio.sampleRateOut,
-    channels: 1
-  });
-
-  audioTest(command, user, config, outputFileStream)
-  .then((data) => {
-    res.sendFile(path.resolve(__dirname, 'test.wav'))
-  })
-  .catch((e) => {
-    console.log(e)
-  })
-    // setTimeout(() =>{
-    //   res.sendFile(audioFile)
-    //   fs.unlink(audioFile, (err) => {
-    //       if (err) console.log(err);
-    //     });
-    // }, 500)
-
-
-})
-
-app.listen(configFile.port, () => console.log(`Firing up the Web Server for communication on address ${ip.address()}:${configFile.port}`))
--- server/routes.js
+++ server/routes.js
@@ -1,2 +1,72 @@
-const express = require('expiress');
-const router = express.Router();
+const express = require('express');
+const routes = express.Router();
+
+const sendTextInput = require('./assistant').sendTextInput;
+const sendAudioInput = require('./assistant').sendAudioInput;
+
+
+routes.post('/assistant', function (req, res) {
+  let command = req.query.command;
+  const broadcast = req.query.broadcast;
+  const user = req.query.user;
+  const converse = req.query.converse;
+  const preset = req.query.preset;
+
+  //if no command passed, return 400
+  if(!command) return res.status(400).json({success: false, error: "No command given"})
+
+  if(preset) {
+    broadcast = true;
+    switch(preset) {
+      case 'wakeup':
+          command = `wake up everyone`;
+          break;
+      case 'breakfast':
+          command= `breakfast is ready`;
+          break;
+      case 'lunch':
+          command = `it's lunch time`;
+          break;
+      case 'dinner':
+          command = `dinner is served`;
+          break;
+      case 'timetoleave':
+          command = `its time to leave`;
+          break;
+      case 'arrivedhome':
+          command = `i'm home`;
+          break;
+      case 'ontheway':
+          command = `i'm on the way`;
+          break;
+      case 'movietime':
+          command = `the movie is about to start`;
+          break;
+      case 'tvtime':
+          command = `the show is about to start`;
+          break;
+      case 'bedtime':
+          command = `we should go to bed`;
+          break;
+      default:
+          command = `you selected a preset broadcast, but didn't say which one`;
+    }
+  }
+
+  // if broadcast is true, pass as broadcast command
+  if(broadcast) command = `broadcast ${command}`;
+
+  sendTextInput(command, user, converse)
+  .then((response) => {
+    res.status(200).json(response);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  })
+})
+
+routes.post('/t', (req, res) => {
+  sendAudioInput();
+})
+
+module.exports = routes;
