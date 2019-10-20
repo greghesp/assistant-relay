ff67c5c3e73738efd98670bf49af47a1637b0197
Greg <greg.hesp@gmail.com>
2019-10-20 19:06:59 +0100

    test

--- relay/app.js
+++ relay/app.js
@@ -21,7 +21,6 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
