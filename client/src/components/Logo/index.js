e5f0090cb3c61a85fcb6b4618b096199c254e057
Greg <greg.hesp@gmail.com>
2019-10-21 21:30:51 +0100

    readme update

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import * as Styles from './styles';
 import logo from '~/static/img/logo.png';
 
+
 function Logo({width, height, margin}) {
     return (
         <Styles.Image margin={margin}>
