1c8fdea3a958fbaf290e2c94a5271471998a215e
Greg <greg.hesp@gmail.com>
2020-07-04 11:15:39 +0100

    sandbox: Bug - Preset doesn't disable on converse checked

--- src/pages/sandbox.js
+++ src/pages/sandbox.js
@@ -47,7 +47,8 @@ const presets = [
 ];
 
 function Sandbox({ users }) {
-  const [json, setJSON] = useState({});
+  const [json, setJSON] = useState({ broadcast: true, converse: false });
+  const [disabled, setDisabled] = useState([]);
 
   return (
     <Dashboard title="Sandbox">
@@ -71,7 +72,7 @@ function Sandbox({ users }) {
                       htmlFor="user"
                       className="block text-sm font-medium leading-5 text-gray-700"
                     >
-                      Users
+                      User
                     </label>
                     <div className="mt-1 rounded-md shadow-sm">
                       <select
@@ -79,12 +80,22 @@ function Sandbox({ users }) {
                         className="form-select block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                         onChange={e => {
                           const u = document.getElementById('user').value;
+                          if (u.length === 0) {
+                            return setJSON($ => {
+                              delete $.user;
+                              return { ...$ };
+                            });
+                          }
                           setJSON($ => ({ ...$, user: u }));
                         }}
                       >
                         <option key="none" />
                         {users.map((u, i) => {
-                          return <option key={i}>{u}</option>;
+                          return (
+                            <option key={i} value={u}>
+                              {u}
+                            </option>
+                          );
                         })}
                       </select>
                     </div>
@@ -98,12 +109,29 @@ function Sandbox({ users }) {
                     </label>
                     <div className="mt-1 rounded-md shadow-sm">
                       <textarea
+                        disabled={disabled.includes('command')}
                         id="command"
                         rows="3"
                         className="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                         onKeyUp={() => {
                           const c = document.getElementById('command').value;
-                          setJSON($ => ({ ...$, command: c }));
+
+                          if (c.length === 0) {
+                            setDisabled($ => {
+                              const a = $.filter(f => f !== 'preset');
+                              return a;
+                            });
+
+                            return setJSON($ => {
+                              delete $.command;
+                              return { ...$ };
+                            });
+                          }
+                          setDisabled($ => [...$, 'preset']);
+                          setJSON($ => {
+                            if (json.preset) delete $.preset;
+                            return { ...$, command: c };
+                          });
                         }}
                       />
                     </div>
@@ -120,12 +148,27 @@ function Sandbox({ users }) {
                     </label>
                     <div className="mt-1 rounded-md shadow-sm">
                       <select
+                        disabled={disabled.includes('preset')}
                         id="preset"
                         className="form-select block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                        onChange={e => {}}
+                        value={json.preset ? json.preset : 'none'}
+                        onChange={e => {
+                          const u = document.getElementById('preset').value;
+                          if (u.length === 0) {
+                            return setJSON($ => {
+                              delete $.preset;
+                              return { ...$ };
+                            });
+                          }
+                          setJSON($ => ({ ...$, preset: u }));
+                        }}
                       >
-                        {presets.map(p => {
-                          return <option key={p.key}>{p.title}</option>;
+                        {presets.map((p, i) => {
+                          return (
+                            <option key={i} value={p.key}>
+                              {p.title}
+                            </option>
+                          );
                         })}
                       </select>
                     </div>
@@ -139,8 +182,14 @@ function Sandbox({ users }) {
                     <div className="absolute flex items-center h-5">
                       <input
                         id="broadcast"
+                        disabled={disabled.includes('broadcast')}
                         type="checkbox"
                         className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                        defaultChecked={json.broadcast}
+                        onChange={() => {
+                          const u = document.getElementById('broadcast').checked;
+                          setJSON($ => ({ ...$, broadcast: u }));
+                        }}
                       />
                     </div>
                     <div className="pl-7 text-sm leading-5">
@@ -157,8 +206,28 @@ function Sandbox({ users }) {
                     <div className="absolute flex items-center h-5">
                       <input
                         id="converse"
+                        disabled={disabled.includes('converse')}
+                        defaultChecked={json.converse}
                         type="checkbox"
                         className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                        onChange={() => {
+                          const u = document.getElementById('converse').checked;
+
+                          if (u) {
+                            setDisabled($ => [...$, 'preset']);
+                            return setJSON($ => ({ ...$, converse: u }));
+                          }
+
+                          setDisabled($ => {
+                            const a = $.filter(f => f !== 'preset');
+                            return a;
+                          });
+
+                          return setJSON($ => {
+                            delete $.converse;
+                            return { ...$ };
+                          });
+                        }}
                       />
                     </div>
                     <div className="pl-7 text-sm leading-5">
@@ -205,9 +274,9 @@ function Sandbox({ users }) {
                   <div className="mt-1 rounded-md shadow-sm">
                     <textarea
                       id="command"
+                      readOnly
                       rows="18"
                       className="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                      defaultValue={JSON.stringify(json, null, 4)}
                       value={JSON.stringify(json, null, 4)}
                     />
                   </div>
